7ec05662fe8e775cad96e3e08f65992c27a41bca
rash <rash.prog@gmail.com>
2013-01-07 01:51:02 +0300

    added arrows

--- jquery.jetscrollpanel.js
+++ jquery.jetscrollpanel.js
@@ -8,7 +8,6 @@
     		var options = $.extend({}, defaults, params);
 
 	        return this.each(function() {
-	        	// if (options.arrows) alert(1);
         		var content = $(this).wrapInner("<div class='jetscrollcontent'/>").find(".jetscrollcontent");
         		
         		var wrapper = content.wrap("<div class='jetscrollpanel'/>").parent();
@@ -16,44 +15,41 @@
         		
         		var bar = wrapper.find(".jetscrollbar");
         		var slider = bar.find(".jetscrollslider");
-        		var bar_diff = bar.height() - slider.height();
         		
-        		/*
         		if (options.arrows) {
-        			bar.prepend("<a href='#' class='jetscrollarrow jetscrollarrow-up'>▲</a>").append("<a href='#' class='jetscrollarrow jetscrollarrow-down'>▼</a>");
+        			bar.prepend("<a href='#' class='jetscrollarrow up'>▲</a>").append("<a href='#' class='jetscrollarrow down'>▼</a>");
         			var h = bar.find(".jetscrollarrow").disableSelection().height();
-        			bar.css({
-        				top: h + "px",
-        				height: (parseInt(bar.height()) - 2 * h) + "px"
-        			});
-
-        			bar.find(".jetscrollarrow").trigger("mousedown", function() {
-        				console.log(1);
-        				if ($(this).is(":active")) {
-        					$(this).click();
-        				} else {
-        					console.log(0);
+        			bar.css("margin", h + "px 0");
+        			
+        			bar.find(".jetscrollarrow").click(function() {
+        				var p = parseInt(slider.css("top"));
+        				if ($(this).hasClass("up")) {
+        					p = Math.max(p - 15, 0);
         				}
-        				return false;
-        				slider.css("top", ($(this).hasClass("jetscrollarrow-up") ? "-" : "+") + "=5%");
-        				wrapper.jetScrollPanel('scroll');
+        				if ($(this).hasClass("down")) {
+        					p = Math.min(p + 15, wrapper.jetScrollPanel("getTrackSize"));
+        				}
+        				slider.css("top", p + "px");
+        				wrapper.jetScrollPanel('scroll', ($(this).hasClass("up") ? "-" : "+") + "=15");
         				return false;
         			});
         		}
-				*/
+				
+
         		slider.draggable({
 		            containment: "parent",
 		            scroll: false,
 		            drag: function(e, ui) {
-        				var p = ui.position.top / bar_diff;
+        				var p = ui.position.top / (wrapper.jetScrollPanel("getTrackSize"));
         				p *= content[0].scrollHeight - content.height();
         				wrapper.jetScrollPanel('scroll', p);
         			}
 		        });
 
-		        content.scroll(function() {
-		        	var p = bar_diff * content.scrollTop() / (content[0].scrollHeight - content.height());
-		            slider.css("top", p + "px");
+		        content.scroll(function(e) {
+		        	var p = (wrapper.jetScrollPanel("getTrackSize")) * content.scrollTop() / (content[0].scrollHeight - content.height());
+		            slider.css("top", Math.floor(p) + "px");
+		            
 		        });
 		        
 		        bar.click(function(e) {
@@ -61,18 +57,12 @@
 		            wrapper.jetScrollPanel('scroll', y > 0 ? "+=15" : "-=15");
 		        });
 
+
         	});
     	},
     	scroll: function(pixels) {
     		var content = $(this).find(".jetscrollcontent");
-    		/*
-    		
-    		var bar = $(this).find(".jetscrollbar");
-    		var slider = bar.find(".jetscrollslider");
-    		var p = parseInt(slider.css("top")) / (bar.height() - slider.height());
-    		p *= content[0].scrollHeight - content.height();
-            content.scrollTop(p);
-            */
+
             if (typeof pixels == "string") {
             	if (pixels.toString().substr(0, 2) == "-=") {
             		pixels = -1 * parseInt(pixels.toString().substr(2));
@@ -83,6 +73,11 @@
             	pixels += content.scrollTop();
             }
             content.scrollTop(pixels);
+    	},
+    	getTrackSize: function() {
+    		var bar = $(this).find(".jetscrollbar");
+    		var slider = bar.find(".jetscrollslider");
+    		return bar.height() - slider.height() - parseInt(slider.css("border-top-width")) - parseInt(slider.css("border-bottom-width"));
     	}
     }
     $.fn.jetScrollPanel = function(method){
